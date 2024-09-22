445a36020770cda8b66593e1604c1ebaff5d4604
Neytred <ls.ivanova91@gmail.com>
2024-09-22 22:11:54 +0900

    Initial commit

--- src/main/java/com/skypro/HW6Collections/exception/EmployeeAlreadyAddedInList.java
+++ src/main/java/com/skypro/HW6Collections/exception/EmployeeAlreadyAddedInList.java
@@ -1,7 +1,13 @@
 package com.skypro.HW6Collections.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyAddedInList extends RuntimeException {
-    public EmployeeAlreadyAddedInList(String message) {
-        super(message);
+
+
+    public EmployeeAlreadyAddedInList(String added) {
     }
 }
--- src/main/java/com/skypro/HW6Collections/exception/EmployeeStorageIsFullList.java
+++ src/main/java/com/skypro/HW6Collections/exception/EmployeeStorageIsFullList.java
@@ -1,7 +1,12 @@
 package com.skypro.HW6Collections.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+
 public class EmployeeStorageIsFullList extends  RuntimeException {
-    public EmployeeStorageIsFullList(String message) {
-        super(message);
+
+    public EmployeeStorageIsFullList(String storageIsFull) {
     }
 }
