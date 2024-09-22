2f87a82ed37cfb63b77304170cb9600b930e0d8e
Neytred <ls.ivanova91@gmail.com>
2024-09-22 21:37:39 +0900

    Initial commit

---
+++ src/main/java/com/skypro/HW6Collections/Hw6CollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.HW6Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw6CollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw6CollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/HW6Collections/Hw6CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.HW6Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw6CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
